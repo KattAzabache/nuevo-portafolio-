1179c36358ed826b97f08593f7c7c45b1c9bfc05
maricieloluna <kattyazabache.z@gmail.com>
2015-06-04 09:24:27 -0500

    coloque un deslize

--- mi-script.js
+++ mi-script.js
@@ -1,3 +1,4 @@
+// menu
 $(document).ready(main);
  
 var contador = 1;
@@ -47,6 +48,31 @@ $(document).ready(function(){
   });
 });
 
+// loading
+$(window).load(function() {
+    // start up after 2sec no matter what
+    window.setTimeout(function(){
+        $('body').removeClass("loading").addClass('loaded');
+    }, 4000);
+});
+
+// deslis lento
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+  $('html,body').animate({
+     scrollTop: target.offset().top - 0 
+    } , 1000);
+      return false;
+    }
+  }
+ });
+});
+
+
 
 // $(document).ready(function(){
 //   $(window).scroll(function(){
@@ -61,12 +87,7 @@ $(document).ready(function(){
 //   });
 // });
 
-$(window).load(function() {
-    // start up after 2sec no matter what
-    window.setTimeout(function(){
-        $('body').removeClass("loading").addClass('loaded');
-    }, 4000);
-});
+
 
 
 
